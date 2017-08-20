dba81232717fd4243ff181fbde081c4907d9fc70
spartanmech <30667918+spartanmech@users.noreply.github.com>
2017-08-20 17:18:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var counter = 0;
-app.get('/',function(req,res){
+app.get('/counter',function(req,res){
     counter = counter+1;
     res.send(counter.toString());
 });
