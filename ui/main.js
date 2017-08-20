a6ddc74bc2a8696ea689d07a1a11dcff3306020d
spartanmech <30667918+spartanmech@users.noreply.github.com>
2017-08-20 16:47:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 //Select the image
 var img = document.getElementById('Jarvis');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft+'px';
+}
 img.onclick = function(){
     img.style.marginLeft = '100px';
 }
