abc35e77e3ec090536a491e2fbe67f69c24d930e
spartanmech <30667918+spartanmech@users.noreply.github.com>
2017-08-20 19:08:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
@@ -55,6 +54,8 @@ submit.onclick = function(){
         //No action yet
     };
     // Make the request
+   var nameInput = document.getElementById('name');
+   var name = nameInput.value  
    request.open('GET','http://spartanhappy98.imad.hasura-app.io/submit-name?name='+ name,true)
    request.send(null);
    
