82a7a8120285145c9d3cdf2b76501ee8dfd77419
spartanmech <30667918+spartanmech@users.noreply.github.com>
2017-08-20 19:05:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,16 +30,36 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    // make a request to the server and send the name
     
-    //capture a list of names and render it as a list
+        var request = new XMLHttpRequest();
     
-    var names = ['name1','name2','name3']
-    var list = '';
-    for (var i =0 ; i<names.length;i++){
-        list+= '<li>' + names[i] + '</li>'
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //capture the response and store it in a variable
+    request.onreadystatechange = function (){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take action
+            if(request.status === 200){
+                 //capture a list of names and render it as a list
     
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i =0 ; i<names.length;i++){
+                    list+= '<li>' + names[i] + '</li>'
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+                            
+                
+            }
+        }
+        //No action yet
+    };
+    // Make the request
+   request.open('GET','http://spartanhappy98.imad.hasura-app.io/submit-name?name='+ name,true)
+   request.send(null);
+   
 };
+   
+    
+   
+    
